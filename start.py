f8e7de6607770c910d4694fc39181ca9be10bdcb
flare <flare.the.protogen.yt@gmail.com>
2025-04-21 15:03:26 -0600

    patch first

--- start.py
+++ start.py
@@ -1,7 +1,8 @@
-from gevent import monkey
-monkey.patch_all()
 import eventlet
 eventlet.monkey_patch()
+from gevent import monkey
+monkey.patch_all()
+input()
 import subprocess
 import threading
 import time
