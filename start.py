f759c53cc0a433fcfb218aea7bc5d9f50b2a9c07
flare <flare.the.protogen.yt@gmail.com>
2025-04-21 09:38:17 -0600

    rename

--- start.py
+++ start.py
@@ -8,5 +8,5 @@ import outlook
 ans = subprocess.Popen(["python","server.py"])
 threading.Thread(target=alertExchange.start_cap_topic_relay, daemon=True).start()
 threading.Thread(target=downloader.downloader, daemon=True).start()
-threading.Thread(target=outlook.start_monitoring, daemon=True).start()
+threading.Thread(target=outlook.start_outlook_watcher, daemon=True).start()
 ans.wait()
