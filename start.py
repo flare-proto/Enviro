1c8ba56f4aa39f414880baa29d6edcde0018a4e0
flare <flare.the.protogen.yt@gmail.com>
2025-04-21 11:07:41 -0600

    delay start to let server load

--- start.py
+++ start.py
@@ -1,11 +1,13 @@
 import subprocess
 import threading
+import time
 
 import alertExchange
 import downloader
 import outlook
 
 ans = subprocess.Popen(["python","server.py"])
+time.sleep(5)
 threading.Thread(target=alertExchange.start_cap_topic_relay, daemon=True).start()
 threading.Thread(target=downloader.downloader, daemon=True).start()
 threading.Thread(target=outlook.start_outlook_watcher, daemon=True).start()
