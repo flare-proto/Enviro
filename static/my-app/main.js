8aa1f4714a22a3b09150c8a6d133d228236c7d10
flare <flare.the.protogen.yt@gmail.com>
2025-04-21 15:35:04 -0600

    cur conds

--- static/my-app/main.js
+++ static/my-app/main.js
@@ -730,7 +730,7 @@ var irtrv = () => {
               .then((rs) => rs.json())
               .then((bft) => {
                   wnd.innerText = `[ ${bft["icon"]} ] ${json["wind_speed"]} km/h at ${json["wind_bearing"]}°`
-                  queuepush(`${json["temperature"]}°C - ${weatherTypes[json["icon_code"]]} - [ ${bft["icon"]} ] ${json["wind_speed"]} km/h at ${json["wind_bearing"]}°`)
+                  queuepush(`Current Conditions: ${json["temperature"]}°C - ${weatherTypes[json["icon_code"]]} - [ ${bft["icon"]} ] ${json["wind_speed"]} km/h at ${json["wind_bearing"]}°`)
               })
       });
       fetch("/api/alerts")
