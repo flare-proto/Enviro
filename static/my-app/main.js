cbab9966b6a44c926e068897cee3e9554804683c
flare <flare.the.protogen.yt@gmail.com>
2025-05-08 08:07:58 -0600

    frontend

--- static/my-app/main.js
+++ static/my-app/main.js
@@ -475,13 +475,13 @@ function styleFunction(feature) {
     }),
   });
 }
-
+const outlooksrc = new VectorSource({
+  url: '/api/outlook/v1',
+  format: new GeoJSON(),
+})
 const outlook_layer = new VectorImageLayer({
   opacity: 1,
-  source: new VectorSource({
-    url: '/api/outlook/v1',
-    format: new GeoJSON(),
-  }),
+  source: outlooksrc,
   style: styleFunction
 })
 
@@ -785,3 +785,10 @@ const ticker      = document.getElementById('ticker');
       setTimeout(playNext, durationMs);
     });
   }
+
+
+
+var outs = document.getElementById("outlookOff")
+outs.onchange = () => {
+  outlooksrc.setUrl(`/api/outlook/v1?offset=${outs.value}`)
+}
